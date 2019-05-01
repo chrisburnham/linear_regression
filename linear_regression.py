94f03c7d780f5d4d4af093f0fd6b69ae2bcc725d
Chris Burnham <cburnham@greensea.com>
2019-05-01 15:33:24 -0400

    optional cols, if not present do all

--- linear_regression.py
+++ linear_regression.py
@@ -1,6 +1,24 @@
 import csv
 import argparse
 
+# Pass in first row to return a list of the header
+# indexs that we care about. If cols is not set 
+# return all of them
+def get_cols_from_headers(row):
+	cols = list()
+	arg_cols = args.get("cols")
+
+	if((type(arg_cols) != type(list())) or (len(arg_cols) == 0)):
+		cols = range(len(row))
+	else:
+		for i in range(len(row)):
+			for name in arg_cols:
+				if(name == row[i]):
+					cols.append(i)
+					break
+
+	return cols
+
 # https://docs.python.org/2/library/csv.html
 # using examples for csv to open file
 def read_csv(filename):
@@ -9,22 +27,15 @@ def read_csv(filename):
 
 		first = True
 
-		# TODO: if we have no cols choose them all
-		#if(len(args.get("cols")) == 0)
-			#first 
-
 		cols = list()
 		for row in spamreader:
 			if(first):
-				for i in range(len(row)):
-					for name in args.get("cols"):
-						if(name == row[i]):
-							cols.append(i)
-
+				cols = get_cols_from_headers(row)
 				first = False
 
 			for i in range(len(row)):
-				# TODO: check if in cols
+				if(cols.count(i) != 0):
+					print row[i]
 
 
 if __name__ == "__main__":
@@ -36,8 +47,7 @@ if __name__ == "__main__":
 	parser.add_argument("--cols",
 											action="append",
 											metavar="Column_name",
-											help="Columns to look at. Specify multiple times",
-											required=True)
+											help="Columns to look at. Specify multiple times")
 
 	# TODO: arg to print headers
 
